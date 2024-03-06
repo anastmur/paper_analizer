38d8491bf13bd43f3338a165673ae8950f98ca3e
Anastasia Muran Trus <anastasia.muran.trus@alumnos.upm.es>
2024-03-06 20:58:55 +0100

    docs configuration

---
+++ docs/conf.py
@@ -0,0 +1,5 @@
+# -- Project information -----------------------------------------------------
+
+project = "Basic Sphinx Example Project"
+copyright = "2022, Read the Docs core team"
+author = "Read the Docs core team"
